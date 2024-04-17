0eacacc8f436a51e50a9180122ae9c7feb1318a5
Katerina Hykova <hkatrina125@gmail.com>
2024-04-17 09:45:32 +0200

    druha funkce

--- sorting.py
+++ sorting.py
@@ -23,10 +23,20 @@ def read_data(file_name):
                     data[key].append(int(row[key]))
     return data
 
+def selection_sort(numbers):
+    for i in range(len(numbers)):
+        min_idx = i
+        for num_idx in range(i + 1, len(numbers)):
+            if numbers[min_idx] > numbers[num_idx]:
+                min_idx = num_idx
+        numbers[i], numbers[min_idx] = numbers[min_idx], numbers[i]
+    return numbers
 
 
 def main():
-    read_data("numbers.csv")
+    numbers = read_data("numbers.csv")
+    sorted_series = selection_sort(numbers["series_1"])
+    print(sorted_series)
 
 
 if __name__ == '__main__':
